546b00033548fcef70a50781c2e118653a511a91
PieterVanhoof <pieter.vanhoof2@gmail.com>
2019-02-14 11:56:08 +0100

    Working app

--- app/src/main/java/ehb/be/mopapp/MainActivity.java
+++ app/src/main/java/ehb/be/mopapp/MainActivity.java
@@ -2,8 +2,10 @@ package ehb.be.mopapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import ehb.be.mopapp.model.MopDAO;
 import ehb.be.mopapp.recyclerutilties.MopAdapter;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,8 +17,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        
+        //verwijzing naar recyler
+        rvMopjes = findViewById(R.id.rv_mop);
+        //adapter, hoe recycler opvullen
+        adapter = new MopAdapter(MopDAO.getInstance().getMopLijst());
+        rvMopjes.setAdapter(adapter);
+        //hoe elementen weergeven (horizontale lijst, verticale lijst?)
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        rvMopjes.setLayoutManager(layoutManager);
 
     }
 }
